4fd3a5f8a42db7499094e7b3660c27ac9520dd14
approx-error-fuxi <juusokaarela1@gmail.com>
2024-11-08 11:30:28 +0200

    Made linecounter.py ignore hidden files by default

--- scripts/linecounter.py
+++ scripts/linecounter.py
@@ -3,13 +3,15 @@
 
 import sys
 import subprocess
+import glob
 
 flags = sys.argv[1:]
 
 output = True
 
+print(a)
 if not flags:
-    output_str = 'Lines of text in current directory:'
+    output_str = 'Lines of text in current directory excluding hidden files:'
     # List directory contents with slashes denoting directories and filter
     # out anything containign a slash thus leaving just the files
     # After that filter the files so that only non-binary files remain
@@ -20,16 +22,39 @@ if not flags:
         del file_list[-1]
     nb_files = ''
     for file in file_list:
-        non_binary = subprocess.run(['grep', '-lI', '.', f'{file}'], capture_output=True, text=True)
+        non_binary = subprocess.run(['grep', '-lI', '.', file], capture_output=True, text=True)
         nb_files += non_binary.stdout
-elif flags and flags[0] in ['-r', '--recursive']:
-    output_str = 'Lines of text in current directory and all subdirectories:'
-    # Search for the wildcard '.' pattern in all non-binary files recursively and return the filename if the pattern is found
-    # Effectively returns a list of all the non-binary files within the directory and its subdirectories
-    non_binaries = subprocess.run(['grep', '-rlI', '.'], capture_output=True, text=True)
-    nb_files = non_binaries.stdout
 else:
-    output = False
+    match flags:
+        case ['-h'] | ['--hidden']:
+            output_str = 'Lines of text in current directory including hidden files'
+            dir_contents = subprocess.run(['ls', '-ap'], capture_output=True, text=True)
+            only_files = subprocess.run(['grep', '-v', '/'], capture_output=True, text=True, input=dir_contents.stdout)
+            file_list = only_files.stdout.split('\n')
+            if file_list[-1] == '':
+                del file_list[-1]
+            nb_files = ''
+            for file in file_list:
+                non_binary = subprocess.run(['grep', '-lI', '.', file], capture_output=True, text=True)
+                nb_files += non_binary.stdout
+        case ['-r'] | ['--recursive']:
+            output_str = 'Lines of text in current directory and all subdirectories excluding hidden ones:'
+            # Search for the wildcard '.' pattern in all non-binary files recursively and return the filename if the pattern is found
+            # Effectively returns a list of all the non-binary files within the directory and its subdirectories
+            # The `ls` makes sure that only non-hidden directories and files are listed. FIXME: This only applies on the first directory level
+            nb_files = ''
+            cd_files = glob.glob('*')
+            for file in cd_files:
+                non_binary = subprocess.run(['grep', '-rlI', '.', file], capture_output=True, text=True)
+                nb_files += non_binary.stdout
+            print(nb_files)
+        case ['-r', '-h'] | ['-h', '-r'] | ['--recursive', '--hidden'] | ['--hidden', '--recursive']:
+            output_str = 'Lines of text in current directory and all subdirectories including hidden ones:'
+            non_binaries = subprocess.run(['grep', '-rlI', '.'], capture_output=True, text=True)
+            nb_files = non_binaries.stdout
+            print(nb_files)
+        case _:
+            output = False
 
 if output:
     nb_list = nb_files.split('\n')
@@ -45,4 +70,5 @@ if output:
     line_count = lines_counted.stdout.strip('\n')
     print(output_str, line_count)
 else:
-    print('\nUSAGE:\n\nList lines of text in current directory:\nlinec\n\nList lines of text in current directory and all subdirectories:\nlinec [-r | --recursive]\n')
+    print('\nUSAGE:\n\nlinec [OPTIONAL FLAGS]\n\nFLAGS:\n\n -r, --recursive: list lines in subdirectories as well\n -h, --hidden: include hidden files and directories.\
+\n\nNOTE: Hidden files and directories are currently only ignored in the starting directory')
